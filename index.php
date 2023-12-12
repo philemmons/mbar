68e9d087eaecbf997316ce07d938f41ae61edb38
philemmons <pemmons@csumb.edu>
2023-12-11 22:55:11 -0800

    navbar save

--- index.php
+++ index.php
@@ -8,28 +8,44 @@
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">Home
+                    <a class="nav-link active" href="index.php" aria-current="page">Home
                         <span class="visually-hidden">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conference</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="mbar2024.php">MBAR 2024</a>
+                        <a class="dropdown-item" href="registration.php">Regristration</a>
+                        <a class="dropdown-item" href="activities.php">Activities</a>
+                        <a class="dropdown-item" href="mbar_history.php">MBAR History</a>
+                        <a class="dropdown-item" href="memories.php">Memories</a>
+                    </div>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Committees</a>
                     <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
+                        <a class="dropdown-item" href="meetings.php">Meetings</a>
+                        <a class="dropdown-item" href="committees.php">Committees</a>
                     </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logo_contest.php">Logo Contest</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contributions.php">Contribution</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
 
-
 <main>
 
 </main>
+
 <?php include 'footer.inc' ?>
 
 <!-- Bootstrap JavaScript Libraries -->
