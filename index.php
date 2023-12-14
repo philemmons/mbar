a06d633f17b43e76eaae4a288cf49b6713a817dd
philemmons <pemmons@csumb.edu>
2023-12-13 21:16:28 -0800

    more layout adj

--- index.php
+++ index.php
@@ -168,7 +168,7 @@
     <!-- Section Three -->
     <section class="container mb-5">
         <div class="row">
-            <div class="col-lg-7 p-4">
+            <div class="col-lg-7 pt-4">
                 <h3>ABOUT MONTEREY BAY AREA ROUNDUP</h3>
                 <p><strong>ALCOHOLICS ANONYMOUS</strong> is a fellowship of people who share their experience, strength and hope with each other that they may solve their common problem and help others to recover from alcoholism. The only requirement for membership is a desire to stop drinking. There are no dues or fees for A.A. membership; we are self-supporting through our own contributions. A.A. is not allied with any sect denomination, politics, organization or institution; does not wish to engage in any controversy; neither endorses nor opposes any causes. Our primary purpose is to stay sober and help other alcoholics to achieve sobriety.
                     <br><br>
