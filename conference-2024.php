13be6c197982839e220f19222643da218452dae0
philemmons <pemmons@csumb.edu>
2023-12-14 17:39:57 -0800

    images filepaths

--- conference-2024.php
+++ conference-2024.php
@@ -8,8 +8,7 @@
         <div class="collapse.true navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php" aria-current="page">Home
-                        <span class="visually-hidden">(current)</span></a>
+                    <a class="nav-link" href="index.php" aria-current="page">Home</a>
                 </li>
                 <li class="nav-item dropdown" style='width: 180px;'>
                     <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-hover="dropdown" aria-haspopup="true" aria-expanded="false">Conference</a>
@@ -45,7 +44,7 @@
 <main>
     <!-- Hero Section -->
     <section class="container shadow-wrap">
-        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-conference-2024" title="MBAR 2024 Conference Center at Portola Plaza, Monterey.">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-conference-2024" title="Monterey Conference Center at Portola Plaza, Monterey.">
             <div class="col-xl-7 col-lg-7 col-md-12 py-5">
                 <div class="p-3 text-center text-bg-light hero-text-border" title="Pre-Registration is open!">
                     <h1 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Monterey Bay Area Roundup 2024</span>
@@ -323,7 +322,7 @@
                             </div>
                         </div>
                         <div class="col-sm-12 col-12 col-xl-12">
-                            <a href="registration.html" class="btn btn-light btn-lg active" role="button" aria-pressed="true"><img src="photo-logo/ButtonReg.png" width="300" alt="" /></a>
+                            <a href="registration.php" class="btn btn-light btn-lg active" role="button" aria-pressed="true"><img src="photo-logo/ButtonReg.png" width="300" alt="" /></a>
                             <br><br>
                             <hr />
                             <p>CHECK IN AT REGISTRATION DESK TO OBTAIN YOUR REGISTRATION PACKET CONTAINING YOUR NAME BADGE,
@@ -347,7 +346,7 @@
                     <div class="row box">
                         <br>
                         <div class="col-sm-11 col-12">
-                            <h3><a href="services.html">Conference info: KEEP IT SIMPLE</a></h3>
+                            <h3><a href="services.php">Conference info: KEEP IT SIMPLE</a></h3>
                         </div>
                         <br>
                         <p><strong>Check in at the registration table</strong> to obtain your registration packets containing your name badges, programs and any tickets for meals or the dance which you may have purchased.
@@ -369,15 +368,15 @@
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                         </div>
                         <div class="col-sm-11 col-12">
-                            <h3><a href="activites.html">WHY DO WE HAVE MBAR?</a></h3>
+                            <h3><a href="activites.php">WHY DO WE HAVE MBAR?</a></h3>
                             <p>Frequent contact with newcomers and with each other is the bright spot of our lives.
-                                <img src="photo-logo/icon/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon, no link just design">
+                                <img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon, no link just design">
                                 <a href="" onclick="window.open('pdf/bigbook/BIG-BOOK-CHAPTER_7_P-89.pdf', 'Sage200Demo', 'width=900,height=768,status=0,resizable=0');return false;"> <strong>Big Book P. 89.</strong></a>
                                 <br>
                                 <br>
                                 It is a fellowship in Alcoholics Anonymous. There you will find release from care, boredom and worry. Your imagination will be fired. <br>
                                 Life will mean something at last! The most satisfactory years of your existence lie ahead. Thus, we find the fellowship, and so will you.
-                                <img src="photo-logo/icon/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon, no link just design">
+                                <img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon, no link just design">
                                 <a href="" onclick="window.open('pdf/bigbook/BIG-BOOK-CHAPTER_11_P-152.pdf', 'Sage200Demo', 'width=900,height=768,status=0,resizable=0');return false;"> <strong>Big Book P. 152</strong></a>
                             </p>
                         </div>
@@ -388,9 +387,9 @@
                     <section id="map"></section>
                     <div class="row box">
                         <div class="col-sm-11  col-xl-12">
-                            <h3><a href="activites.html">Bethlehem Lutheran Church</a></h3>
+                            <h3><a href="activites.php">Bethlehem Lutheran Church</a></h3>
                             <div class="col-md-12 text-left" id="bordermap">
-                                <strong><img src="photo-logo/icon/warning-sign-vector.png" width="50" height="50" alt="">PLEASE NOTE:</strong> The Breakfast and Dinner will be held at<br><strong> Bethlehem Lutheran Church, located at 800 Cass Street in Monterey</strong> (a little over one mile from the Conference Center).<br><br>
+                                <strong><img src="images/icons/warning-sign.png" width="50" height="50" alt="">PLEASE NOTE:</strong> The Breakfast and Dinner will be held at<br><strong> Bethlehem Lutheran Church, located at 800 Cass Street in Monterey</strong> (a little over one mile from the Conference Center).<br><br>
 
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3203.345532490949!2d-121.89797058430084!3d36.59398628697924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808de425e4f48de1%3A0xc2ec674814c2393f!2sBethlehem+Lutheran+Church!5e0!3m2!1sen!2sus!4v1550676666728" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
                             </div>
@@ -398,7 +397,7 @@
                             <hr>
                             <br>
                             <section id="mcc"></section>
-                            <h3><a href="activites.html">Monterey Conference Center</a></h3>
+                            <h3><a href="activites.php">Monterey Conference Center</a></h3>
                             <div class="col-md-12 text-left" id="bordermap1">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3203.037184010839!2d-121.89686518419717!3d36.6014134799899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808de422461f891b%3A0x20f8f64ee124f091!2sMonterey+Conference+Center!5e0!3m2!1sen!2sus!4v1548347273704" width="100%" height="450" style="border:0" allowfullscreen=""></iframe>
                             </div>
