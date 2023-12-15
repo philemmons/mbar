842e6881ac70d2602da705d27bbd62e9cab8633e
philemmons <pemmons@csumb.edu>
2023-12-15 12:39:59 -0800

    activity page

---
+++ activities.php
@@ -0,0 +1,169 @@
+<?php include 'header.inc' ?>
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse.true navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item dropdown" style='width: 180px;'>
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-hover="dropdown" aria-haspopup="true" aria-expanded="false">Conference</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="conference-2024.php">MBAR 2024</a>
+                        <a class="dropdown-item" href="registration.php">Registration</a>
+                        <a class="dropdown-item active" href="activities.php">Activities<span class="visually-hidden">(current)</span></a>
+                        <a class="dropdown-item" href="mbar_history.php">MBAR History</a>
+                        <a class="dropdown-item" href="memories.php">Memories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown" style='width: 161px;'>
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-hover="dropdown" aria-haspopup="true" aria-expanded="false">Committees</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="meetings.php">Meetings</a>
+                        <a class="dropdown-item" href="committees.php">Committees</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logo_contest.php">Logo Contest</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item" style="border-right: none;">
+                    <a class="nav-link" href="contributions.php">Contribution</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <!-- Hero Section -->
+    <section class="container shadow-wrap">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-activity" title="Beautiful Monterey Bay sunset and other activities.">
+            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="Join us in the fellowship.">
+                    <h1 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Monterey Bay Area Roundup<br>
+                            Activities</span></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section One -->
+    <section class="container shadow-wrap">
+        <div class="row">
+            <div class="col-lg-4 col-sm-12 col-12">
+                <figure class="figure">
+                    <img src="../images/speaker-on-stage.png" class="figure-img img-fluid" alt="Speaker sharing their experience, strength, and hope with us.">
+                    <figcaption class="figure-caption">
+                        <h2> Guest Speakers</h2>
+                        <p>At the main speaker meetings our Speakers share their experience, strength and hope. "Showing others who suffer how we were given help is the very thing which makes life seem so worth while to us now. Cling to the thought that, in God's hands, the dark past is the greatest possession you have - the key to life and happiness for others." BB p124
+                        </p>
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col-lg-8 col-sm-12 col-12">
+                <div class="row">
+                    <div class="col-sm-6 col-12">
+                        <figure class="figure">
+                            <img src="../images/con-dinner.png" class="figure-img img-fluid" alt="Dinner with the speaker on Saturday night.">
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <h4>Meet the Speakers Dinner</h4>
+                        <h5><strong>at Bethlehem Lutheran Church</strong>.
+                            <p><a href="https://mbar.philemmons.dev/conference-2022.php#map">(See map here)</a>
+                            </p>
+                            Join us for a delicious dinner and meet the speakers at the Bethlehem Lutheran Church Hall (in the back of the building).
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-12">
+                        <figure class="figure">
+                            <img src="../images/meeting-in-progress.png" class="figure-img img-fluid" alt="Recovering people are attending a marathon meeting.">
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <h4>Marathon Meetings</h4>
+                        <h5>During the MBAR Conference there will be meetings held that are open to all A.A. Members at various hours during the day. Marathon meetings will not be held during the Conference's Main A.A. / Al-Anon Speaker Meetings. You do not have to be registered for the conference to attend these meetings.
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-12">
+                        <figure class="figure">
+                            <img src="../images/con-breakfast.png" class="figure-img img-fluid" alt="Join us for a Sunday morning breakfast.">
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <h4>Roundup Breakfast</h4>
+                        <p><a href="https://mbar.philemmons.dev/conference-2022.php#map">(See map here)</a></p>
+                        <h5>Join us for a delicious breakfast and fellowship <strong>at Bethlehem Lutheran Church</strong>. (in the back of the building) to start off the activities of the day.
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-12">
+                        <figure class="figure">
+                            <img src="../images/con-dance.png" class="figure-img img-fluid" alt="A silhouette of fun and fellowship at the Saturday night dance.">
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <h4>Dance and Fellowship</h4>
+                        <h5>After the Main speaker meeting on Saturday Night, join us for some fun and fellowship. After all, "we aren't a glum lot. If newcomers could see no joy or fun in our existence, they wouldn't want it. We absolutely insist on enjoying life." BB p132
+
+                        </h5>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6 col-12">
+                            <figure class="figure">
+                                <img src="../images/con-ice-cream.png" class="figure-img img-fluid mt-4" alt="Ice cream social logo with the words on top of a cone.">
+                            </figure>
+                        </div>
+                        <div class="col-sm-6 col-12">
+                            <h4>Ice-Cream Social</h4>
+                            <h5><b>Will be held on the Stevenson Terrace at the Conference Center</b>. <p><a href="https://mbar.philemmons.dev/conference-2022.php#map">(See map here)</a></p><br>
+                                Stop what you are doing, and come have some ice cream, with your favorite toppings! Enjoy the fellowship and share about your experience at being at this year's MBAR with other members.<br>
+                                For more info, pick up a program at the Registration desk!
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Section One -->
+    <section class="container shadow-wrap">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-6 col-md-12 pt-4">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="Preregistration is open!">
+                    <h3 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Monterey Bay Area Roundup 2024</span>
+                    </h3>
+                    <p class="mb-6 h4 text-dark">August 31st and<br>September 1st</p>
+                    <a href="registration.php" class="btn btn-primary me-2">Register Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+</main>
+<?php include 'footer.inc' ?>
+</body>
+
+</html>
