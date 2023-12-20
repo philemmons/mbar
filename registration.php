59601c788e696515b7fe29fc9229cd3ef861fb44
philemmons <pemmons@csumb.edu>
2023-12-20 10:39:43 -0800

    reposition link

--- registration.php
+++ registration.php
@@ -58,11 +58,11 @@
     <!-- Section One -->
     <section class="container shadow-wrap">
         <div class="row justify-content-center mb-5">
+            <div id="register-now"></div>
             <div class="col-xl-8 col-lg-10 col-md-12 py-4">
                 <div class="p-3 text-center text-bg-light hero-text-border" title="Preregistration is open!">
-                    <div id="register-now"></div>
-                    <div style= "height: 50rem;">
-                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf6ouc-DIkpARsMa5rmX7HmLy2dwejBr79fDQ9aqUbR--DDQQ/viewform?embedded=true" width= "100%" height= "100%" marginheight="0" marginwidth="0">Loading…</iframe>
+                    <div style="height: 50rem;">
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf6ouc-DIkpARsMa5rmX7HmLy2dwejBr79fDQ9aqUbR--DDQQ/viewform?embedded=true" width="100%" height="100%" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
                 </div>
             </div>
