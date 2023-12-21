9742dd335c119ffb41e27f44b475c187a9434881
philemmons <pemmons@csumb.edu>
2023-12-21 06:32:23 -0800

    revert changes

--- memories.php
+++ memories.php
@@ -165,8 +165,8 @@
                             </div>
                         </div>
 
-                        <div class="col-md-6 text-center data-sitekey=<? echo getenv('g-site-key'); ?> ">
-                            <button type="submit" class="btn btn-primary g-recaptcha" name="memory_submit" id="submitBtn" data-callback="onRecaptchaSuccess" data-action="submit" disabled>Submit</button>
+                        <div class="col-md-6 text-center">
+                            <button type="submit" class="btn btn-primary g-recaptcha" name="memory_submit" id="submitBtn" data-sitekey=<? echo getenv('g-site-key'); ?> data-callback="onRecaptchaSuccess" data-action="submit" disabled>Submit</button>
                         </div>
                       <!--
                         <div class="col-md-6 text-center">
