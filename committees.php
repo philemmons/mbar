e0f50103c995f12590f805dacc5d1034cd3a7880
philemmons <pemmons@csumb.edu>
2023-12-22 09:26:41 -0800

    colspan="4" for header

--- committees.php
+++ committees.php
@@ -78,7 +78,7 @@
                     </tbody>
                     <thead class="filterHead">
                         <tr class="filterRow">
-                            <th>AA Steering Committee</th>
+                            <th colspan="4">AA Steering Committee</th>
                         </tr>
                     </thead>
 
@@ -116,7 +116,7 @@
                     </tbody>
                     <thead class="filterHead">
                         <tr class="filterRow">
-                            <th>Al-Anon Steering Committee</th>
+                            <th colspan="4">Al-Anon Steering Committee</th>
                         </tr>
                     </thead>
 
@@ -198,7 +198,7 @@
 
                     <thead class="filterHead">
                         <tr class="filterRow">
-                            <th>Committee Chairs</th>
+                            <th colspan="4">Committee Chairs</th>
                         </tr>
                     </thead>
 
