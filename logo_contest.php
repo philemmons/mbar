df13433ff2ba0d0d27e2098a303f46bf630d84c3
philemmons <pemmons@csumb.edu>
2023-12-22 10:00:50 -0800

    logo contest content added

---
+++ logo_contest.php
@@ -0,0 +1,92 @@
+<?php include 'header.inc' ?>
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse.true navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdownId" data-bs-hover="dropdown" aria-haspopup="true" aria-expanded="false">Conference</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="conference-2024.php">MBAR 2024</a>
+                        <a class="dropdown-item" href="registration.php">Registration</a>
+                        <a class="dropdown-item" href="activities.php">Activities</a>
+                        <a class="dropdown-item" href="mbar_history.php">MBAR History</a>
+                        <a class="dropdown-item" href="memories.php">Memories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdownId" data-bs-hover="dropdown" aria-haspopup="true" aria-expanded="false">Committees</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="meetings.php">Meetings</a>
+                        <a class="dropdown-item" href="committees.php">Committees</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="logo_contest.php">Logo Contest<span class="visually-hidden">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item" style="border-right: none;">
+                    <a class="nav-link" href="contributions.php">Contribution</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <!-- Hero Section -->
+    <section class="container shadow-wrap">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-logoContest" title="">
+            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="We care.">
+                    <h1 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Logo Contest<br>
+                            2024</span></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section One -->
+    <section class="container shadow-wrap">
+        <div class="row justify-content-center mb-5">
+
+            <div class="col-xl-8 col-lg-8 col-md-10 pt-4">
+                <div class="p-3 text-center text-bg-light hero-text-border">
+                    <h4 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Micha C. - Logo D, is MBAR's Logo Contest winner for 2024.</span>
+                    </h4>
+                    <p class="mb-6 h5 text-dark">Great job Micha!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Two -->
+    <section class="container shadow-wrap">
+        <div class="row justify-content-center py-4 mb-5">
+            <div class="col-sm-12 col-12 text-center px-3">
+                <img class="img-fluid" src="../images/logo2024.png" alt="Twelve different logo's were submitted for 2024 with the theme of Keep It Simple.">
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-6 col-md-12 pt-4">
+                <div class="p-3 text-center text-bg-light hero-text-border">
+                    <p class="mb-6 h4 text-dark">Thank you to all who participated. We hope you enjoyed this contest as much as we did.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+<?php include 'footer.inc' ?>
+</body>
+
+</html>
