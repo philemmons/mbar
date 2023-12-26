d274edcaa13015328e54e8909b090619337804a7
philemmons <philemmons.now@gmail.com>
2023-12-25 20:05:25 -0800

    left align formal intent

--- accessibility.php
+++ accessibility.php
@@ -57,7 +57,7 @@
     <section class="container shadow-wrap">
         <div class="row justify-content-center mb-5">
             <div class="col-xl-10 col-lg-10 col-md-12 pt-4">
-                <div class="p-3 text-center text-bg-light hero-text-border" title="Our belief of inclusiveness nurtures our desire for recovery">
+                <div class="p-3 text-bg-light hero-text-border" title="Our belief of inclusiveness nurtures our desire for recovery">
                     <h4 class="fw-bold mb-3 text-primary">
                         <span class="text-dark px-3 px-md-0">MBAR's General Accessibility Statement</span>
                     </h4>
@@ -65,7 +65,7 @@
                 </div>
             </div>
             <div class="col-xl-10 col-lg-10 col-md-12 py-4">
-                <div class="p-3 text-center text-bg-light hero-text-border" title="">
+                <div class="p-3 text-bg-light hero-text-border" title="">
 
                     <h4 class="fw-bold mb-3 text-primary">
                         <span class="text-dark px-3 px-md-0">Accessibility on montereybayarearoundup.org</span>
