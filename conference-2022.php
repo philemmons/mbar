8b44726ecbb12c6fb23ed68f8cd8a41ae395f783
philemmons <philemmons.now@gmail.com>
2023-12-27 09:59:22 -0800

    test5

--- conference-2022.php
+++ conference-2022.php
@@ -57,25 +57,18 @@
     <section class="container shadow-wrap">
         <div class="row justify-content-center">
             <div class="col-xl-10 col-lg-10 col-md-12 pt-4">
-                <div class="p-3 text-bg-light hero-text-border" title="">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="">
+                    <div class='col-sm-4 banner'>
+                        <h3 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">2022 A.A. Conference with Al-Anon Participation</h1></span>
+                        </h3>
+                        <p class="mb-6 h4 text-dark">Featuring Speaker's Audio</p>
+                    </div>
+                    <div class='col-sm-auto banner'>
+                        <img src="../images/logo2022.png" class="img-fluid shadow-wrap" alt="Monterey Bay Are Roundup 2022 logo with a sea otter and the slogan: Keep It Simple" title="Artist Unknown" />
 
-                    <h3 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">2022 A.A. Conference with Al-Anon Participation</h1></span>
-                    </h3>
-                    <p class="mb-6 h4 text-dark">Featuring Speaker's Audio</p>
-                
-                    <img src="../images/logo2022.png" class="img-fluid shadow-wrap" alt="Monterey Bay Are Roundup 2022 logo with a sea otter and the slogan: Keep It Simple" title="Artist Unknown" />
+                    </div>
+
                 </div>
-
-            </div>
-        </div>
-        <div class="row justify-content-md-center">
-            <div class='col-sm-4 banner'>
-                <img src="../images/logo2022.png" class="img-fluid shadow-wrap" alt="Monterey Bay Are Roundup 2022 logo with a sea otter and the slogan: Keep It Simple" title="Artist Unknown" />
-            </div>
-            <div class='col-sm-8 col-md-6 p-4'>
-                <h3 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">2022 A.A. Conference with Al-Anon Participation</h1></span>
-                </h3>
-                <p class="mb-6 h4 text-dark">Featuring Speaker's Audio</p>
             </div>
         </div>
         <div class="row justify-content-center mb-5">
