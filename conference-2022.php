588d66bac506b60d57c01d039cda6255cdad8b11
philemmons <philemmons.now@gmail.com>
2023-12-27 11:00:36 -0800

    one more formatting

--- conference-2022.php
+++ conference-2022.php
@@ -59,7 +59,7 @@
             <div class='col-sm-auto banner pt-4'>
                 <img src="../images/logo2022.png" class="img-fluid shadow-wrap" alt="Monterey Bay Are Roundup 2022 logo with a sea otter and the slogan: Keep It Simple" title="Artist Unknown" />
             </div>
-            <div class='col-xl-6 col-lg-6 col-md-10 banner pt-4'>
+            <div class='col-xl-8 col-lg-8 col-md-10 banner pt-4'>
                 <h3 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">2022 A.A. Conference with Al-Anon Participation</h1></span>
                 </h3>
                 <p class="mb-6 h4 text-dark">Featuring Speaker's Audio</p>
