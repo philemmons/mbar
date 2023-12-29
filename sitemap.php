956a33644417d28e04a2f7fd8a04806feee7e295
philemmons <philemmons.now@gmail.com>
2023-12-28 18:06:50 -0800

    more formatting and css

--- sitemap.php
+++ sitemap.php
@@ -52,8 +52,8 @@
 		<div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-sitemap" title="Virtual network on a navy blue background.">
 			<div class="col-xl-7 col-lg-7 col-md-12 py-5">
 				<div class="p-3 text-center text-bg-light hero-text-border" title="HTML Site Map - assist with indexing">
-					<h1 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">montereybayarearoundup.org<br>HTML Site Map</span>
-					</h1>
+					<h3 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">montereybayarearoundup.org<br>HTML Site Map</span>
+					</h3>
 				</div>
 			</div>
 		</div>
@@ -61,58 +61,56 @@
 
 	<section class="container shadow-wrap">
 		<div class="row justify-content-center mb-5">
-			<div class="col-xl-10 col-lg-10 col-md-12 py-4">
-				<div id="top">
-					<div class="row py-4">
-						<div class="col-md-6">
-							<h5><a href="http://mbar.philemmons.dev">mbar.philemmons.dev Homepage</a>
-							</h5>
-						</div>
-						<div class="col-md-6">
-							<h5>Last updated: 2023, December 28
-								<br>
-								Total pages: 18
-							</h5>
-						</div>
-					</div>
+			<div id="top">
+				<div class="col-xl-6 col-lg-6 col-md-12 pt-4">
+					<h5><a href="http://mbar.philemmons.dev">mbar.philemmons.dev Homepage</a>
+					</h5>
 				</div>
-				<div id="cont">
-					<ul class="level-0">
-						<li class="lhead">https:/ </li>
+				<div class="col-xl-6 col-lg-6 col-md-12 pt-4">
+					<h5>Last updated: 2023, December 28
+						<br>
+						Total pages: 18
+					</h5>
+				</div>
+			</div>
+		</div>
+
+		<div id="cont">
+			<ul class="level-0">
+				<li class="lhead">https:/ </li>
+				<li>
+					<ul class="level-1">
+						<li class="lhead">/ </li>
 						<li>
-							<ul class="level-1">
-								<li class="lhead">/ </li>
-								<li>
-									<ul class="level-2">
-										<li class="lhead">mbar.philemmons.dev/ <span class="lcount">18 pages</span></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/" title="Monterey Bay Area Roundup 2024">Monterey Bay Area Roundup 2024</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/index.php" title="Monterey Bay Area Roundup 2024 | Home">Monterey Bay Area Roundup 2024 | Home</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/conference-2024.php" title="Monterey Bay Area Roundup 2024 | Conference 2024">Monterey Bay Area Roundup 2024 | Conference 2024</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/registration.php" title="Monterey Bay Area Roundup 2024 | Registration">Monterey Bay Area Roundup 2024 | registration</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/activities.php" title="Monterey Bay Area | Activities">Monterey Bay Area Roundup 2024 | activities</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/mbar_history.php" title="Monterey Bay Area | MBAR History">Monterey Bay Area Roundup 2024 | MBAR History</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/memories.php" title="Monterey Bay Area | Memories">Monterey Bay Area Roundup 2024 | Memories</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/meetings.php" title="Monterey Bay Area | Meetings">Monterey Bay Area Roundup 2024 | Meetings</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/committees.php" title="Monterey Bay Area | Committees">Monterey Bay Area Roundup 2024 | Committees</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/upcoming_event.php" title="Monterey Bay Area | Upcoming Events">Monterey Bay Area Roundup 2024 | Upcoming Events</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/logo_contest.php" title="Monterey Bay Area | Logo Contest">Monterey Bay Area Roundup 2024 | Logo Contest</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/contact.php" title="Monterey Bay Area | Contact Us">Monterey Bay Area Roundup 2024 | Contact Us</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/contributions.php" title="Monterey Bay Area | Contributions">Monterey Bay Area Roundup 2024 | Contributions</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/conference-2022.php" title="Monterey Bay Area | Archive Conference 2022">Monterey Bay Area Roundup 2024 | Archive Conference 2022</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/accessibility.php" title="Monterey Bay Area | Accessibility">Monterey Bay Area Roundup 2024 | Accessibility</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/privacy-policy.php" title="Monterey Bay Area | Privacy Policy">Monterey Bay Area Roundup 2024 | Privacy Policy</a></li>
-										<li class="lpage"><a href="https://mbar.philemmons.dev/sitemap.php" title="Monterey Bay Area | Sitemap">Monterey Bay Area Roundup 2024 | Sitemap</a></li>
-										<li class="lpage last-page"><a href="https://mbar.philemmons.dev/conference-2019.php" title="Monterey Bay Area | Archive Conference 2019">Monterey Bay Area Roundup 2024 | Archive Conference 2019</a></li>
-									</ul>
-								</li>
+							<ul class="level-2">
+								<li class="lhead">mbar.philemmons.dev/ <span class="lcount">18 pages</span></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/" title="Monterey Bay Area Roundup 2024">Monterey Bay Area Roundup 2024</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/index.php" title="Monterey Bay Area Roundup 2024 | Home">Monterey Bay Area Roundup 2024 | Home</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/conference-2024.php" title="Monterey Bay Area Roundup 2024 | Conference 2024">Monterey Bay Area Roundup 2024 | Conference 2024</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/registration.php" title="Monterey Bay Area Roundup 2024 | Registration">Monterey Bay Area Roundup 2024 | registration</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/activities.php" title="Monterey Bay Area | Activities">Monterey Bay Area Roundup 2024 | activities</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/mbar_history.php" title="Monterey Bay Area | MBAR History">Monterey Bay Area Roundup 2024 | MBAR History</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/memories.php" title="Monterey Bay Area | Memories">Monterey Bay Area Roundup 2024 | Memories</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/meetings.php" title="Monterey Bay Area | Meetings">Monterey Bay Area Roundup 2024 | Meetings</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/committees.php" title="Monterey Bay Area | Committees">Monterey Bay Area Roundup 2024 | Committees</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/upcoming_event.php" title="Monterey Bay Area | Upcoming Events">Monterey Bay Area Roundup 2024 | Upcoming Events</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/logo_contest.php" title="Monterey Bay Area | Logo Contest">Monterey Bay Area Roundup 2024 | Logo Contest</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/contact.php" title="Monterey Bay Area | Contact Us">Monterey Bay Area Roundup 2024 | Contact Us</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/contributions.php" title="Monterey Bay Area | Contributions">Monterey Bay Area Roundup 2024 | Contributions</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/conference-2022.php" title="Monterey Bay Area | Archive Conference 2022">Monterey Bay Area Roundup 2024 | Archive Conference 2022</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/accessibility.php" title="Monterey Bay Area | Accessibility">Monterey Bay Area Roundup 2024 | Accessibility</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/privacy-policy.php" title="Monterey Bay Area | Privacy Policy">Monterey Bay Area Roundup 2024 | Privacy Policy</a></li>
+								<li class="lpage"><a href="https://mbar.philemmons.dev/sitemap.php" title="Monterey Bay Area | Sitemap">Monterey Bay Area Roundup 2024 | Sitemap</a></li>
+								<li class="lpage last-page"><a href="https://mbar.philemmons.dev/conference-2019.php" title="Monterey Bay Area | Archive Conference 2019">Monterey Bay Area Roundup 2024 | Archive Conference 2019</a></li>
 							</ul>
 						</li>
 					</ul>
-					<!-- Please note: You are not allowed to remove the copyright notice below. Thank you! www.xml-sitemaps.com -->
-				</div>
-				<div id="sitemap-footer">
-					Page created with <a target="_blank" href="https://www.xml-sitemaps.com">Google XML sitemap and html sitemaps generator</a> | Copyright &copy; 2005-2023 XML-Sitemaps.com
-				</div>
+				</li>
+			</ul>
+			<!-- Please note: You are not allowed to remove the copyright notice below. Thank you! www.xml-sitemaps.com -->
+
+			<div id="sitemap-footer">
+				Page created with <a target="_blank" href="https://www.xml-sitemaps.com">Google XML sitemap and html sitemaps generator</a> | Copyright &copy; 2005-2023 XML-Sitemaps.com
 			</div>
 		</div>
 	</section>
