f912a278c2c3e9327651cce1260791b9273575db
philemmons <philemmons.now@gmail.com>
2023-12-30 19:37:02 -0800

    hr test

--- _registration.php
+++ _registration.php
@@ -356,7 +356,7 @@
                             <p class="h5"> À la carte Meal Options - If you prefer to purchase meals individually, please select from the following options.</p>
                         </div>
 
-                        <div class="col-lg-6">
+                        <div class="col-lg-4">
                             <label for="reg-mtsd" class="form-label">Meet The Speakers Dinner (Saturday Night @ 5:00pm) - $25</label>
                             <select class="form-select" name="reg-mtsd" id="reg-mtsd" required>
                                 <option selected disabled value="">Choose...</option>
@@ -370,7 +370,7 @@
                         </div>
 
 
-                        <div class="col-lg-6">
+                        <div class="col-lg-4">
                             <label for="reg-rucb" class="form-label">Round-Up Continental Breakfast (Sunday Morning @ 8:00am - 9:30am) - $10</label>
                             <select class="form-select" name="reg-rucb" id="reg-rucb" required>
                                 <option selected disabled value="">Choose...</option>
@@ -384,7 +384,7 @@
                         </div>
 
 
-                        <div class="col-lg-6">
+                        <div class="col-lg-4">
                             <label for="reg-ics" class="form-label">Ice Cream Social (Sunday Afternoon @ 3:00pm - 4:00pm) - $5</label>
                             <select class="form-select" name="reg-ics" id="reg-ics" required>
                                 <option selected disabled value="">Choose...</option>
@@ -396,6 +396,8 @@
                                 Please select one.
                             </div>
                         </div>
+
+                      <hr><hr>
 
                         <div class="col-lg-6">
                             <label for="reg-snd" class="form-label">Saturday Night Dance @ Conference Center (FREE with paid registration or $5 at the door)</label>
@@ -409,7 +411,6 @@
                                 Please select one.
                             </div>
                         </div>
-
 
                         <div class="col-lg-6">
                             <label for="reg-hhc" class="form-label">Helping Hand Contribution - Please consider making a contribution to ensure all who desire to attend are able.</label>
@@ -427,6 +428,8 @@
                             </div>
                         </div>
 
+
+<hr><hr>
 
                         <div class="col-lg-6">
                             <label for="reg-pm" class="form-label">Payment Method - <strong>Registration incomplete until paid</strong> - Payment options below</label>
