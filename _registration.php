8fc7b57885bddd3b0963c7d5d6d9124dbfd97502
philemmons <philemmons.now@gmail.com>
2023-12-30 20:03:18 -0800

    changes

--- _registration.php
+++ _registration.php
@@ -387,7 +387,8 @@
                             </div>
                         </div>
                         
-                    
+                        <hr>
+                        
                         <div class="col-lg-6">
                             <label for="reg-snd" class="form-label">Saturday Night Dance @ Conference Center (FREE with paid registration or $5 at the door)</label>
                             <select class="form-select" name="reg-snd" id="reg-snd" required>
@@ -416,7 +417,7 @@
                             </div>
                         </div>
 
-                     
+                        <hr>
 
                         <div class="col-md-6">
                             <div class="form-check">
