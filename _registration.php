3c5cbcf1d459099ec47867e2067a3d9b2a93e13f
philemmons <philemmons.now@gmail.com>
2023-12-31 22:19:35 -0800

    JS CHECKS

--- _registration.php
+++ _registration.php
@@ -395,7 +395,7 @@
 
                         <div class="col-lg-4">
                             <label for="reg-mtsd" class="form-label">Meet The Speakers Dinner (Saturday Night @ 5:00pm)* - $25 </label>
-                            <select class="form-select" name="reg-mtsd" id="reg-mtsd" onchange= "optionALACART()" required>
+                            <select class="form-select" name="reg-mtsd" id="reg-mtsd" onChange="optionMTSD(this)" required>
                                 <option selected disabled value="">Choose...</option>
                                 <option value="yes">Yes, please!</option>
                                 <option value="no">No thank you</option>
@@ -408,7 +408,7 @@
 
                         <div class="col-lg-4">
                             <label for="reg-rucb" class="form-label">Round-Up Continental Breakfast (Sunday Morning @ 8:00am - 9:30am)* - $10</label>
-                            <select class="form-select" name="reg-rucb" id="reg-rucb" onchange= "optionALACART()" required>
+                            <select class="form-select" name="reg-rucb" id="reg-rucb" onChange="optionRUCB(this)" required>
                                 <option selected disabled value="">Choose...</option>
                                 <option value="yes">Yes, please!</option>
                                 <option value="no">No thank you</option>
@@ -421,7 +421,7 @@
 
                         <div class="col-lg-4">
                             <label for="reg-ics" class="form-label">Ice Cream Social (Sunday Afternoon @ 3:00pm - 4:00pm)* - $5</label>
-                            <select class="form-select" name="reg-ics" id="reg-ics" onchange= "optionALACART()" required>
+                            <select class="form-select" name="reg-ics" id="reg-ics" onChange="optionICS(this)" required>
                                 <option selected disabled value="">Choose...</option>
                                 <option value="yes">Yes, please!</option>
                                 <option value="no">No thank you</option>
@@ -653,25 +653,36 @@ form.onchange = updateForm;
 
     }
 
-    function optionSND(obj) {
+    function optionSND($obj) {
         let snd = document.getElementById("reg-snd");
-        if (obj.checked)
+        if ($obj.checked)
             snd.value = "yes";
         else
             snd.value = "";
     }
 
-    function optionALACART() {
+    function optionMTSD($obj) {
+        let ebmb = document.getElementById('reg-ebmb');
+        if ($obj.value == "yes")
+            ebmb.value = "no";
+        else
+            ebmb.value = "";
+    }
 
-        let myArr = ['reg-mtsd', 'reg-rucb', 'reg-ics'];
+    function optionRUCB($obj) {
+        let ebmb = document.getElementById('reg-ebmb');
+        if ($obj.value == "yes")
+            ebmb.value = "no";
+        else
+            ebmb.value = "";
+    }
 
-        myArr.forEach(function($key) {
-            if ($key.value == "yes")
-                document.getElementById('reg-ebmb').value = "no";
-            else
-                document.getElementById('reg-ebmb').value = "";
-        });
-
+    function optionICS($obj) {
+        let ebmb = document.getElementById('reg-ebmb');
+        if ($obj.value == "yes")
+            ebmb.value = "no";
+        else
+            ebmb.value = "";
     }
 </script>
 </body>
