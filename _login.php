bb711fe493077c3de334ed6e9368980238a3e90c
philemmons <pemmons@csumb.edu>
2024-01-10 22:15:42 -0800

    get base modal working

--- _login.php
+++ _login.php
@@ -98,6 +98,11 @@ if (isset($_POST['logout'])) {
             <h4 class="fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Please login to continue...</span>
             </h4>
             <br>
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
             <button onclick="document.getElementById('id01').style.display='block'" class="btn btn-primary">Login</button>
             <?php
             if (isset($_POST['login'])) {
@@ -131,6 +136,26 @@ if (isset($_POST['logout'])) {
     </div>
   </form>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <?php include_once 'footer.inc' ?>
 
