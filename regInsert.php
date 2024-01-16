0bf91fd17e1a12255679ae94432d707bbbc0c917
philemmons <pemmons@csumb.edu>
2024-01-16 15:25:25 -0800

    change insert banner

--- regInsert.php
+++ regInsert.php
@@ -149,7 +149,7 @@ if (isset($_POST['submitInsert'])) {  //admin has submitted the "new user" form
 
     <!-- Hero Section -->
     <section class="container shadow-wrap">
-        <div class="row justify-content-center py-6 bg-body-tertiary bg-img-update" title="Seychelles beach during a beautiful day by Chris Blaichch.">
+        <div class="row justify-content-center py-6 bg-body-tertiary bg-img-insert" title="Hiking in the Pfeiffer Big Sur and Julia Pfeiffer Burns State Parks.">
             <div class="col-xl-7 col-lg-7 col-md-12 py-5">
                 <div class="p-3 text-center text-bg-light hero-text-border">
                     <h1 class="display-6 fw-bold mb-3 text-primary"><span class="text-dark px-3 px-md-0">Add New Registration</span>
