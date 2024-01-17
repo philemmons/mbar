fd2d87b3be5de387d0b8a61d8eb97880fd99e46c
philemmons <pemmons@csumb.edu>
2024-01-16 22:04:13 -0800

    update alt and title

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ include_once 'header.inc'
                 </p>
             </div>
             <div class="col-lg-4 text-center">
-                <img src="../images/coastal_wave.png" class="img-fluid shadow-wrap" alt="Huge wave crashing on the rocky shore.">
+                <img src="../images/coastal_wave.png" class="img-fluid shadow-wrap" alt="Gorgeous, satin blue Pacific Ocean sunset with waves washing on the rocky shore." title= "Gorgeous, satin blue Pacific Ocean sunset with waves washing on the rocky shore.">
             </div>
         </div>
     </section>
