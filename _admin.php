c27a50a2fbd1c316e11c2020bf7d0896ef05598a
philemmons <pemmons@csumb.edu>
2024-01-30 21:09:02 -0800

    limit the DT js jq library

--- _admin.php
+++ _admin.php
@@ -391,6 +391,11 @@ function displayRegAdmin($registration)
 
   <?php include_once 'footer.inc' ?>
 
+<!-- https://datatables.net/ -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+  
   <script>
     //https://datatables.net/reference/option
     new DataTable('#adminDisplay', {
