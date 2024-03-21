82c1056c11cc6c885bdb5b7b667fab4f07551838
philemmons <pemmons@csumb.edu>
2024-03-21 13:05:07 -0700

    slider testing

--- mbar_history.php
+++ mbar_history.php
@@ -133,14 +133,17 @@ include_once 'header-bottom.inc'
                                     <img src="../images/1993.png" class="d-block w-100" alt="MBAR 1993">
                                 </div>
                             </div>
+
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-prev-icon"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
+
                             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-next-icon"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
+
                         </div>
                     </section>
                 </div>
