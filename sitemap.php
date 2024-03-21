babf5fc18a14a76705b388fb704f4c93e357fd3b
philemmons <pemmons@csumb.edu>
2024-03-21 15:47:21 -0700

    url typo

--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,7 @@
 session_start();  //start or resume an existing session
 include_once 'header-top.inc';
 echo "<title>MBAR - Sitemap </title>";
-echo '<link rel="canonical" href= "https://monterey.bayarearoundup.org/sitemaps.php" />';
+echo '<link rel="canonical" href= "https://monterey.bayarearoundup.org/sitemap.php" />';
 include_once 'header-bottom.inc'
 ?>
 
