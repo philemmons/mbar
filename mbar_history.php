f6b27a5c48114bdfa66b7ec123b98648fe29677d
philemmons <pemmons@csumb.edu>
2024-03-21 13:36:33 -0700

    buttons ada

--- mbar_history.php
+++ mbar_history.php
@@ -134,13 +134,13 @@ include_once 'header-bottom.inc'
                                 </div>
                             </div>
 
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon"></span>
+                            <button class="btn btn-primary btn-sm" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
 
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                                <span class="carousel-control-next-icon"></span>
+                            <button class="btn btn-primary btn-sm" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
 
