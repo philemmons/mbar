0248d164441519e1c877746e39c3fa41f81023bb
philemmons <pemmons@csumb.edu>
2024-03-21 19:56:41 -0700

    test

---
+++ con-2017-paul-o.php
@@ -0,0 +1,37 @@
+<?php
+session_start();  //start or resume an existing session
+include_once 'header-top.inc';
+echo "<title>MBAR - 2017 Paul O. </title>";
+echo '<link rel="canonical" href= "https://monterey.bayarearoundup.org/con-2017-paul-o.php" />';
+include_once 'header-bottom.inc'
+?>
+
+<main id="main-content">
+    <!-- Hero Part -->
+    <div class="container shadow-wrap">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-audio-trans" title="Retro microphone on a blue background banner">
+            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="Audio Speaker Transcript">
+                    <h2 class="display-6 fw-bold text-primary"><span class="text-dark px-3 px-md-0">Audio Transcript<br>Raw and Unedited</span></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Part One -->
+    <article aria-label="Paul-O-Transcript">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-10 col-lg-10 col-md-12 py-4">
+                    <div class="p-3 text-bg-light hero-text-border">
+                        <section aria-label="Transcript Paul O. 2017">
+                            <h3 class="card-title text-center">Transcript Paul O.<br>2017</h3>
+                            <p>
+                                Test
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
