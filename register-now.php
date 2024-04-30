8d5d4c22529b7b37ac4aa285a385fa9ef89e9334
philemmons <pemmons@csumb.edu>
2024-04-30 15:54:36 -0700

    test lightbox

--- register-now.php
+++ register-now.php
@@ -847,6 +847,30 @@ include_once 'header-bottom.inc'
             }
         }
     </script>
+    <script>
+        const lightbox = document.createElement('div')
+        lightbox.id = 'lightbox'
+        document.body.appendChild(lightbox)
+
+        const images = document.querySelectorAll('.l-box')
+        images.forEach(image => {
+            image.addEventListener('click', e => {
+                lightbox.classList.add('active')
+                const img = document.createElement('img')
+                img.src = image.src
+                while (lightbox.firstChild) {
+                    lightbox.removeChild(lightbox.firstChild)
+                }
+                lightbox.appendChild(img)
+            })
+        })
+
+        lightbox.addEventListener('click', e => {
+            if (e.target !== e.currentTarget) return
+            lightbox.classList.remove('active')
+        })
+    </script>
+
     </body>
 
     </html>
