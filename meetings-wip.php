c76d6c3c0206a10a999f4adeca6aad97e84bed64
philemmons <pemmons@csumb.edu>
2024-05-02 16:47:11 -0700

    css

--- meetings-wip.php
+++ meetings-wip.php
@@ -90,8 +90,8 @@ include_once 'header-bottom.inc'
         <div class="container shadow-wrap">
             <div class="row justify-content-center mb-5">
                 <div class="col-xl-8 col-lg-8 col-md-10 py-4">
-                    <div class="p-3 text-center text-bg-light hero-text-border">
-                        <h4 class="fw-bold mb-3"><span class="text-dark px-3 px-md-0"><i class="bi bi-book me-2" style="font-size: 1.5rem; color: var(--color-8);" aria-hidden="true"></i>2024 Meeting Minutes</span>
+                    <div class="p-3  text-bg-light hero-text-border">
+                        <h4 class="fw-bold mb-3 text-center"><span class="text-dark px-3 px-md-0"><i class="bi bi-book me-2" style="font-size: 1.5rem; color: var(--color-8);" aria-hidden="true"></i>2024 Meeting Minutes</span>
                         </h4>
                         <hr>
 
@@ -189,7 +189,6 @@ include_once 'header-bottom.inc'
                                 </div>
                             </div>
                         </div>
-
 
                     </div>
                 </div>
