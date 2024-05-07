08d9ba6fd51732cc0b335155f5056dea70284705
philemmons <pemmons@csumb.edu>
2024-05-07 16:16:45 -0700

    guess what it is - closing tag

--- es/register-now.php
+++ es/register-now.php
@@ -78,7 +78,7 @@ include_once 'header-bottom.inc'
                         <section aria-label="Registration-Intro">
                             <h3 class="card-title text-center mb-3">Registro 2024</h3>
                             <p>Nos complace anunciar que el Resumen del Área de la Bahía de Monterey se realizará en 2024, ¡y nos alegra que planee asistir! La conferencia se llevará a cabo en el Centro de Conferencias de Monterey el fin de semana del Día del Trabajo: del 31 de Agosto al 1 de Septiembre de 2024. Después de registrarse, debería recibir un correo electrónico con sus opciones y el total adeudado.</p>
-                            <section>
+                        </section>
                     </div>
                 </div>
 
