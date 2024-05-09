df60ea283940625d0256c24705a2c5104eefcd80
philemmons <pemmons@csumb.edu>
2024-05-09 12:01:17 -0700

    code check in

--- meetings-wip.php
+++ meetings-wip.php
@@ -97,44 +97,44 @@ include_once 'header-bottom.inc'
                         <h4 class="fw-bold mb-3 text-center"><span class="text-dark px-3 px-md-0"><i class="bi bi-book me-2" style="font-size: 1.5rem; color: var(--color-8);" aria-hidden="true"></i>2024 Meeting Minutes</span>
                         </h4>
                         <hr>
-
-
-
                         <div class="accordion accordion-flush" id="accordion2024">
                             <div class="accordion-item">
                                 <h4 class="accordion-header">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#march2024" aria-expanded="false" aria-controls="march2024">
-                                        Accordion Item #1
-                                    </button>
+                                    <button class="accordion-button collapsed text-center" type="button" data-bs-toggle="collapse" data-bs-target="#april2024" aria-expanded="false" aria-controls="april2024">April 13th</button>
                                 </h4>
-                                <div id="march2024" class="accordion-collapse collapse" data-bs-parent="#accordion2024">
-                                    <div class="accordion-body">
-                                        <!--  NOTE: <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.-->
-                                    </div>
-                                </div>
+
+                                <?php include 'meeting-2024-04.inc'; ?>
+
                             </div>
+
                             <div class="accordion-item">
                                 <h4 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#february2024" aria-expanded="false" aria-controls="february2024">
-                                        Accordion Item #2
-                                    </button>
+                                    <button class="accordion-button collapsed text-center" type="button" data-bs-toggle="collapse" data-bs-target="#march2024" aria-expanded="false" aria-controls="march2024">March 9th</button>
                                 </h4>
-                                <div id="february2024" class="accordion-collapse collapse" data-bs-parent="#accordion2024">
-                                    <div class="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                    </div>
-                                </div>
+
+                                <?php include 'meeting-2024-03.inc'; ?>
+
                             </div>
+
+                            <div class="accordion-item">
+                                <h4 class="accordion-header">
+                                    <button class="accordion-button collapsed text-center" type="button" data-bs-toggle="collapse" data-bs-target="#february2024" aria-expanded="false" aria-controls="february2024">February 10th</button>
+                                </h4>
+
+                                <?php include 'meeting-2024-02.inc'; ?>
+
+                            </div>
+
                             <div class="accordion-item">
                                 <h4 class="accordion-header">
                                     <button class="accordion-button collapsed text-center" type="button" data-bs-toggle="collapse" data-bs-target="#january2024" aria-expanded="false" aria-controls="january2024">January 13th</button>
                                 </h4>
 
-                                <?php include 'meeting-2024-jan.inc'; ?>
+                                <?php include 'meeting-2024-01.inc'; ?>
 
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
