bcc501c4ec7757d9fb2a03fa087bd71c87e0b14c
philemmons <pemmons@csumb.edu>
2024-07-30 16:05:52 -0700

    mbar history pdf links

--- mbar_history.php
+++ mbar_history.php
@@ -203,16 +203,16 @@ include_once 'header-bottom.inc'
                                     <i class="bi bi-award me-2" style="font-size: 2rem; color: var(--color-8);" aria-hidden="true"></i>Past Fliers Over the Years
                                 </h3>
                                 <p class="d-grid gap-2">
-                                    <button type="button" class="btn btn-outline-primary btn-sm" onclick="window.open('pdf/MBAR-2015-Program.pdf', 'Sage200Demo', 'width= 900, height= 768, status= 0, resizable= 0');return false;"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2015 Program</button>
+                                    <a class="btn btn-outline-primary btn-sm" href="pdf/MBAR-2015-Program.pdf"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2015 Program<span class="sr-only">(Open pdf in browser)</span></a>
                                 </p>
                                 <p class="d-grid gap-2">
-                                    <button type="button" class="btn btn-outline-primary btn-sm" onclick="window.open('pdf/MBAR-2014-Program.pdf', 'Sage200Demo', 'width= 900, height= 768, status= 0, resizable= 0');return false;"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2014 Program</button>
+                                    <a class="btn btn-outline-primary btn-sm" href="pdf/MBAR-2014-Program.pdf"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2014 Program<span class="sr-only">(Open pdf in browser)</span></a>
                                 </p>
                                 <p class="d-grid gap-2">
-                                    <button type="button" class="btn btn-outline-primary btn-sm" onclick="window.open('pdf/MBAR-2003-Program.pdf', 'Sage200Demo', 'width= 900, height= 768, status= 0, resizable= 0');return false;"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2003 Program</button>
+                                    <a class="btn btn-outline-primary btn-sm" href="pdf/MBAR-2003-Program.pdf"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 2003 Program<span class="sr-only">(Open pdf in browser)</span></a>
                                 </p>
                                 <p class="d-grid gap-2">
-                                    <button type="button" class="btn btn-outline-primary btn-sm" onclick="window.open('pdf/MBAR-1997-Program.pdf', 'Sage200Demo', 'width= 900, height= 768, status= 0, resizable= 0');return false;"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 1997 Program</button>
+                                    <a class="btn btn-outline-primary btn-sm" href="pdf/MBAR-1997-Program.pdf"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon"> 1997 Program<span class="sr-only">(Open pdf in browser)</span></a>
                                 </p>
                             </section>
                         </div>
