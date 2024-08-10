6ddc5799961d4956ef7ce1a59bf8ec042ee0908a
philemmons <pemmons@csumb.edu>
2024-08-09 23:24:41 -0700

    DT test3

--- marathon-2024.php
+++ marathon-2024.php
@@ -234,7 +234,7 @@ include_once 'header-bottom.inc'
                 <section aria-label="Sunday's events">
                     <h3 class="white-on-black py-2 mt-2 text-center">Sunday, September 1st</h3>
 
-                    <table class="table table-bordered table-striped table-hover display nowrap" style="width:100%;" aria-labelledby="tableTwo" id="mmSunDisplay>
+                    <table class="table table-hover display nowrap" style="width:100%;" aria-labelledby="tableTwo" id="mmSunDisplay>
                         <caption class="small" id="tableTwo">A.A Marathon Meetings for Sunday - The first row consist of four columns which are Time, Name, City, and Topic. The first column has the starting time for each meeting. There are single columns spanning all four columns that are non marathon meeting events with time and event name.</caption>
 
                         <thead class="table-light">
