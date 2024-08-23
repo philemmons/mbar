62ae2b7cb1460aff376366381bea107cf2c786d3
philemmons <pemmons@csumb.edu>
2024-08-22 21:36:49 -0700

    content; layout; init

---
+++ aa-spanish-marathon-2024.php
@@ -0,0 +1,355 @@
+<?php
+session_start();  //start or resume an existing session
+include_once 'header-top.inc';
+echo "<title>MBAR - A.A. Spanish Marathon Meetings 2024</title>";
+echo '<link rel="canonical" href= "https://montereybayarearoundup.org/aa-spanish-marathon-2024.php" >';
+include_once 'header-bottom.inc'
+?>
+
+<nav class="navbar navbar-expand-lg" aria-label="main navigation">
+    <div class="container">
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-conference" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conference</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-conference">
+                        <a class="dropdown-item" href="conference-2024.php">MBAR 2024</a>
+                        <a class="dropdown-item active" aria-current="page" href="aa-spanish-marathon-2024.php">A.A. Spanish Marathon Meetings<span class="visually-hidden">(current)</span></a>
+                        <a class="dropdown-item" href="aa-marathon-2024.php">A.A. English Marathon Meetings</span></a>
+                        <a class="dropdown-item" href="al-anon-marathon-2024.php">Al-Anon Marathon Meetings</a>
+                        <a class="dropdown-item" href="register-now.php">Registration</a>
+                        <a class="dropdown-item" href="activities.php">Activities</a>
+                        <a class="dropdown-item" href="mbar_history.php">MBAR History</a>
+                        <a class="dropdown-item" href="memories.php">Memories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-who-we-are" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Who We Are</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-who-we-are">
+                        <a class="dropdown-item" href="our_partner.php">Our Partner</a>
+                        <a class="dropdown-item" href="meetings.php">Meetings</a>
+                        <a class="dropdown-item" href="committees.php">Committees</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-upcoming-events" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Upcoming Events</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-upcoming-events">
+                        <a class="dropdown-item" href="upcoming_event.php">Gratitude</a>
+                        <a class="dropdown-item" href="logo_contest.php">Logo Contest</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item" style="border-right: none;">
+                    <a class="nav-link" href="helping_hands.php">Helping Hands</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main id="main-content">
+    <!-- Hero Part -->
+    <div class="container shadow-wrap">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-conference-2024" title="Monterey Conference Center - Portola Plaza, Monterey.">
+            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="2024 A.A. Marathon Meetings">
+                    <h2 class="display-6 fw-bold px-3 px-md-0">2024 A.A. Spanish Marathon Meetings</h2>
+                    <p class="mb-6 h5 text-dark">Throughout Labor Day weekend, we will have A.A and Al-Anon meetings in Spanish and English.
+                        <br>
+                        <strong>Marathon Meetings are open to <em>all</em>, no registration is required.</strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Part One -->
+    <article aria-label="A.A. Marathon Meetings Title">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-10 col-md-12 py-4">
+                    <div class="p-3 text-center text-bg-light hero-text-border" title="Registration is Live">
+                        <section aria-label="A.A. Marathon Schedule">
+                            <h3 class="fw-bold text-dark px-3 px-md-0">A.A. Spanish Marathon Schedule</h3>
+                            <p class="mb-6 h5 text-dark">Presenting our current lineup. If you would like to be of service, or have a question, then please contact our Spanish Chair: Roberto or Jose.
+                                <br><br>
+                                God,<br>
+                                Grant me the Serenity to Accept the Things I cannot change,
+                                <br>
+                                the Courage to Change the Things I can,
+                                <br>
+                                and the Wisdom to KNow the Difference.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+
+    <!-- Part Two-->
+    <article id="schedule" aria-label="A.A. Meetings - Spanish">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-10 col-lg-10 col-md-12 py-4">
+                    <div class="p-3 text-center text-bg-light hero-text-border">
+
+                        <section aria-label="Saturday's Spanish A.A. Meetings">
+                            <h3 class="white-on-black p-2 mt-2 text-center">Spanish-Speaking Alcoholics Anonymous Programming MBAR Saturday August 31, 2024</h3>
+                            <div class="row g-0 mb-4">
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>First Meeting</strong>
+                                </div>
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>10:00am to 11:30am</strong>
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Coordinator: Jesus
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Treasurer: Angel
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Martin - Resureccion
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Gratitude
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Adrian - El Salvador
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Live Day by Day
+                                </div>
+
+
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>Second Meeting</strong>
+                                </div>
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>1:30pm to 3:00pm</strong>
+                                </div>
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Coordinator: Artemio
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Treasurer: Geolfredo
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Manuel Fierros
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Move Away From the First Drink
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Omar - Nuevo Amanecer
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: What is Sobriety?
+                                </div>
+
+                            </div>
+                        </section>
+
+                        <section aria-label="Sunday's A.A. Meetings">
+                            <h3 class="white-on-black p-2 mt-2 text-center">Spanish-Speaking Alcoholics Anonymous Programming MBAR Saturday September 1st, 2024</h3>
+                            <div class="row g-0 mb-4">
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>First Meeting</strong>
+                                </div>
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>10:00am to 11:30am</strong>
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Coordinator: Raul
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Treasurer: Norberto
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Hector - Alborada
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Participation and Action
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Fernando - San Jose
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Belief in a Higher Power
+                                </div>
+
+
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>Second Meeting</strong>
+                                </div>
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>1:00pm to 2:30pm</strong>
+                                </div>
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Coordinator: Joel
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Treasurer: Jaime
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                 Sofia - Alborada
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Service
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                   Claudia - La Desicion de to Vida
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Dependence
+                                </div>
+
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>Third Meeting</strong>
+                                </div>
+
+                                <div class="col-sm-12 text-center marathon-cell">
+                                    <strong>3:00pm to 4:30pm</strong>
+                                </div>
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Coordinator: Mario
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Treasurer: Jesus Corza
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                    Esmeralda - 3 Legacies
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Sponsorship
+                                </div>
+
+
+                                <div class="col-sm-6 top marathon-cell">
+                                   Juan - Oxford
+                                </div>
+                                <div class="col-sm-6 bottom marathon-cell">
+                                    Topic: Ways to Carry the A.A. Message
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-12 p-2 mt-2">
+                                    <strong>Disclaimer: This schedule is subject to change.</strong>
+                                </div>
+                            </div>
+
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+
+
+    <!-- Part Five -->
+    <aside aria-label="Why we have marathon meetings">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-10 col-lg-10 col-md-12 py-4">
+                    <div class="p-3 text-center text-bg-light hero-text-border">
+                        <section aria-label="Why marathon meetings?">
+                            <h4 class="card-title mb-3">Why A.A. Has Marathon Meetings During Conventions?</h4>
+                            <p>Marathon meetings are a crucial part of A.A. conventions because they provide continuous support and fellowship for attendees. These meetings offer a space for members to share their experiences, strength, and hope around the clock, ensuring that help is always available when needed.
+                            </p>
+                            <p>For many, conventions can be emotionally intense, triggering memories or feelings that require immediate attention and support. Marathon meetings address this by providing a safe, welcoming environment where members can connect with others at any time, day or night. This continuous availability is vital in helping members stay grounded, especially those who may be struggling with cravings, emotions, or difficult experiences during the convention.
+                            </p>
+                            <p>Additionally, marathon meetings foster a sense of community and inclusivity, ensuring that everyone, regardless of their schedule, has the opportunity to participate in meetings. They symbolize the commitment of A.A. to be there for its members whenever they need it, reinforcing the principles of unity and mutual aid that are at the heart of the A.A. program.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+
+    <!-- Part Seven -->
+    <aside aria-label="ADA accessibility and inclusion.">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-10 col-lg-10 col-md-12 py-4">
+                    <div class="p-3 text-bg-light hero-text-border">
+                        <section aria-label="Event unity">
+                            <h4 class="card-title mb-3 text-center text-uppercase">ADA Accessibility</h4>
+                            <p>Our team is committed to ensure the success of your event. Please discuss any areas not covered in this document with your Event Manager or our Staff. These general policies, rules and regulations are subject to change.
+                            </p>
+                            <p><a href="https://www.montereyconferencecenter.com/facility/ada-accessibility/" class="bb-link" target="_blank" rel="noopener noreferrer nofollow"><strong>The Monterey Conference Center ADA compliance policy</strong><span class="sr-only">(Opens a new window)</span></a> - The Monterey Conference Center (MCC) is ADA compliant. In accordance with the ADA, we are responsible for accommodations associated with permanent premise access, such as, but not limited to, wheelchair ramps, elevator standards, door width standards and restroom accessibility.
+                            </p>
+                            <p>It is the client or renter's responsibility to accommodate non-permanent accessibility requirements and incorporate visually-assisted devices, interpreters, and temporary seating accessibility if needed.
+                            </p>
+                            <p>Assisted hearing devices are available through our preferred in-house Audio-Visual provider.
+                            </p>
+                            <p><strong><span class="text-uppercase">ADA Accessibility</span> - MBAR 2024 will provide two ASL Interpreters on these days:</strong>
+                            </p>
+                            <ul>
+                                <li>Friday - 1.5 hours</li>
+                                <li>Saturday - 3.0 hours</li>
+                                <li>Sunday - 3.0 hours</li>
+                            </ul>
+                            <p style="font-style: italic;"><strong class="text-uppercase">Please note:</strong> ADA accessibility's are subject to change with the new schedule, reservation times, and location.
+                            </p>
+                            <p class='sr-only'>(This is the above text without italics)><strong class="text-uppercase">Please note:</strong> ADA accessibility's are subject to change with the new schedule, reservation times, and location.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+
+    <?php include_once 'footer.inc' ?>
+    </body>
+
+    </html>
