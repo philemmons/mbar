2d4fbb8991893eda6ba9c510cc37f5c7fc80072e
philemmons <philemmons.now@gmail.com>
2024-08-30 13:56:57 -0700

    add sch events

--- conference-2024.php
+++ conference-2024.php
@@ -97,6 +97,11 @@ include_once 'header-bottom.inc';
                                 Please note that registration is required for the in-person event.
                             </p>
                             <p class="dark-text mb-6">We are excited to have you join us! Please come over to the registration table to sign up in person, as online registration is now closed. We can't wait to welcome you!</p>
+
+                            <p class="h3 text-dark">Schedule of Events!</p>
+                            <p class="d-grid gap-2">
+                                <a class="btn btn-outline-primary btn-sm" href="pdf/MBAR-2024-Program.pdf"><img src="images/icons/pdf-logo.png" width="30" height="30" alt="Adobe PDF icon">MBAR's Program 2024<span class="sr-only">(Open pdf in browser)</span></a>
+                            </p>
                         </section>
 
                     </div>
