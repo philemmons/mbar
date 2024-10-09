0062f9ff44e9e5f8f445425802922e0c302b8669
philemmons <pemmons@csumb.edu>
2024-10-09 12:51:39 -0700

    pasquale complete es en

--- con-2024-pasquale.php
+++ con-2024-pasquale.php
@@ -27,7 +27,59 @@ include_once 'header-bottom.inc';
                         <section aria-label="transcript">
                             <h3 class="text-center p-4">Below is a raw transcript from the audio recording by<br>Pasquale C. in 2024.<span class="sr-only">(Click to go back)</span>
                             </h3>
-                            <p> Transcript Coming Soon </p>
+                            <p>Good morning everyone, I'm Pascual, I'm an alcoholic. My sobriety day is May 6, 2023 and I'm extremely happy to be here. Before I go in, just a little bit about myself, I'm originally from Atlantic City, New Jersey. I'm out here right now stationed at MPS in the United States Navy and I got sober out here, which I'm internally grateful for. And before I go into my talk, I did want to read a quick paragraph out of chapter 4 of the big book to hopefully I'm going to tie everything back in during my talk.</p>
+
+                            <p>Sorry, can I get my timer set up? Alright, okay, so chapter 4. We need to ask ourselves but one short question, do I believe or am I willing to believe that there is a power greater than myself? As soon as a man can say that he does believe or is willing to believe, we emphatically assume him that he is on his way. It has been repeatedly proven among us that upon this simple cornerstone, the most effective spiritual structure can be built.</p>
+
+                            <p>So when I first walked into the rooms of AA, before I walked in, I did some research. I was like, you know what, I'm not going to get blindsided by people. I'm like, I'm going to get become an expert. So I went on the internet, I'm like, alright, cool. I'm going to, okay, 12 steps, got it, sounds good.</p>
+
+                            <p>Alright, we can do that. It's like, oh, got it, got it. Yeah, I did 12 years of Catholic school. I know God. And when I first sat down with my sponsor and we started taking steps together, we got to step three and he said, he was like, hey, do you believe in God?
+
+                            <p>I was like, yeah, of course. Since I was a kid. But do you put your faith in God? That's a big difference. And he quotes this in every word he speaks and I try to quote it as well as, you know, God doesn't want a lot from you.</p>
+
+                            <p>He just wants all of you. And I said, okay, what does that mean? No idea. And working through step three, I had found myself arrogant in saying that, yeah, I believe in God, but I'm not willing to give myself to him. I'm not willing to put my heart into him.</p>
+
+                            <p>And when we were talking, I'm very much a doer. Up until this point, May 6 is actually got arrested, but before that, I did a lot of, I was at the top of my game, right? And whether it be in the Navy, I was getting an alcohol never got in the way of anything. And I was doing all the right things. And I was very much like, I did that.</p>
+
+                            <p>God didn't help me with that. And, you know, obviously I was wrong and very arrogant to think that. But when we started doing that and going through, he's like, are you doing all this amazing things? Are you doing all the amazing things? Because you're a good person or you want to feel good about yourself?
+
+                            <p>I was like, wow, like, yeah, I mean, I was being very selfish and everything I was doing. Even though I was doing things for people and helping them out and, you know, which translates me getting rewarded, it was really all about me, which I didn't want. And it's not, and looking back, it's like, wow, like, I actually did that. So I remember he said before we stopped on step three, he's like, hey, before we jump in, he's like, I want you to go home. He's like, I want you to find a prayer, whether it be a prayer or a big book, we'll find your own prayer.</p>
+
+                            <p>That really symbolizes yourself and how you want to attack this program. I said, okay. And up until that point, I graduated from high school, Catholic school, and I really didn't pray that much. I didn't go to church, I didn't do the basic necessities, right, of, you know, believing in God. And I was like, I don't know what prayer I'm going to choose.</p>
+
+                            <p>I don't know. So I started Googling a bunch of stuff and I was like, this isn't working. And I remember every day in school, I don't know how I remember it, but I mean, I think it was just because we said it every day before we started our classes in high school. And I'm not going to go into the full prayer, but at the very end, it says, you've made it for yourself, O Lord, and I heart your restless to the rest in you. And that last line, I heart your restless to the rest in you, my heart was restless.</p>
+
+                            <p>I was irritable, discontent, angry, and I said, I was like, man, that's, I'm not putting myself in the God, and that's why I'm hurting. That's why I can't get through all these things in my life. That's why, you know, I turn to the turn to alcohol. And we, I immediately called my sponsor and I was like, hey, I think I got it. So the next day we met and we, you know, continued taking the steps, but going into that and, you know, how that affects my daily life.</p>
+
+                            <p>I didn't, before or after high school, I didn't pray every day. I didn't wake up and say, my, you know, our father, whatever prayer, or anything. I just went about my day. I said, you know what, if I'm going to go attack today, I'm going to do this. I don't need anybody's help.</p>
+
+                            <p>I got this. This is all to me. You know, if I, if I believe that I can do it, then I can do it. And ever since I, you know, got through step three and started working out the rest of the steps, I do wake up every day and pray. And it's, it's funny how so many little things have happened to me since I've taken, I've taken this plunge into this, you know, amazing community that I can't explain them.</p>
+
+                            <p>And there's only one explanation. It was God's very, very much working in my life every single day. And it could be the little list of things, whether, for example, today, I drive a pretty big truck and I have a hard time fine parking. And believe it or not, I was like going to all the different parking garages. My truck doesn't fit because I usually, you know, I usually park in the one over by the war.</p>
+
+                            <p>And I was like, you know what, let me just try the parking lot right next to the conference center. Probably going to be full, but you know, let's just do it. If not, I'll be late. I'll text somebody. It'll be okay.</p>
+
+                            <p>And once you know, at the very last spot, in the very top of the parking garage was open. I'm like, oh, my God, cool. It's going to be, it's going to be a good day. But little things like that that I can't explain is just like it just ends up working out the way that it is. And going from step three, taking those steps.</p>
+
+                            <p>And now I do want to shift to step 10. Because that's something, again, that I intertwined with step three and being able to pray every single day and taking that daily inventory and that daily reflection of how I was. So in the morning, right, when I wake up, I do my, I take, I do my prayer. I pray. And then as we keep going throughout the day, I drop things down in my mind and say like, okay, did I handle that situation?
+
+                            <p>Correctly. Did I do ever, did I do, did I do God's will? Did I go out, do the right thing for the right reason and not for me? Or did I, you know, blow up on somebody for no reason and be, you know, arrogant and jerk and all the bad things? And when I do lay my head on that pillow at night, I can sit back and think and I say, okay, cool.</p>
+
+                            <p>I either I did or I didn't, how am I going to be better the next day? God, how are you going to make me better the next day? You know, because it's not just me anymore, right? It's, we're in this together, me and God, and my higher power, and I couldn't be more grateful for that. Because it's funny.</p>
+
+                            <p>So when I, when I did get arrested and I stopped drinking, I went nine months of white knuckling about myself. And I was like, you know what, like easy day. I can just stop drinking and all my problems will be solved. Perfect. That's how, how wrong I was.</p>
+
+                            <p>And those nine months were absolutely miserable for me. Yeah, I didn't touch a drink. I didn't touch a beer. Didn't touch a cocktail. But still, I was extremely angry for no reason.</p>
+
+                            <p>The simplest of things, someone cut me off and, you know, on the highway. I didn't get the grade I wanted in school. Whatever, whatever, whatever it could be. And then when I started walking in these rooms in January of this year, it was a complete 180 of my personality and complete 180 of myself. And one, I think the people of the fellowship, but two, it's, you know, me putting myself in the God and letting him take the reins, you know, like God, I will support you in any way that you seem fit.</p>
+
+                            <p>You give me the obstacles. I will work through them with you. But giving that trust to him, giving that faith to him, you know, and I think sometimes we have faith. And people say, you know, I have faith. I believe in God.</p>
+
+                            <p>But are you doing your part in what God's word is? What God's will is? And those are kind of the biggest things that really affect my daily living. And how I, you know, interact with people, how I do things. And it's an ongoing challenge.</p>
+
+                            <p>And I'm very excited. You know, I'm only, I'm very, very early in my sobriety of the year and three months. But with the help of, you know, the fellowship to help my higher power, it's, it makes every day go by quicker. And it makes every day go by a little bit more simpler. So thank you very much for inviting me and for the panel, for inviting me up here to speak today.</p>
+
+                            <p>And I'm for Squally and I'm a great block of all.</p>
                         </section>
                     </div>
                 </div>
