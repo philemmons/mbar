4f2f2178a8e9d54f58e2c67e048bfde159f82993
philemmons <pemmons@csumb.edu>
2024-10-09 12:16:43 -0700

    jill complete es en

--- con-2024-jill.php
+++ con-2024-jill.php
@@ -27,7 +27,85 @@ include_once 'header-bottom.inc';
                         <section aria-label="transcript">
                             <h3 class="text-center p-4">Below is a raw transcript from the audio recording by<br>Jill V. in 2024.<span class="sr-only">(Click to go back)</span>
                             </h3>
-                            <p> Transcript Coming Soon </p>
+                            <p>Thank you everyone. Hi, my name is Jill, grateful alcoholic. And actually, I didn't know what the format was. Chris asked me last night if I would do this. Here I am. Last minute thing. And I forgot my phone.</p>
+
+                            <p>So can somebody poke me? Literally poke me. Because I don't hear well. He had to really poke me. Anyway, I am really grateful to be here. Thank you, Chris. And thank you, sweetheart.</p>
+
+                            <p>OK, I'll probably can't see very well either. OK. Anyway, like I said, I'm so grateful to be here. And I have to tell you, it takes me back to 1993 when we started this thing. And what a miracle it's been. I was sitting with Jerry. He's still alive, so I won't say his last name.</p>
+
+                            <p>In the Alotto Club, when it was upstairs. And I guess we don't have an Alotto Club anymore. What's it called today? Felicia Paul, right. Anyway, we were sitting up there. We have both moved here. Talk about moving from another place.</p>
+
+                            <p>It's scary when you're sober. Anyway, we were sitting up there and we thought, we both came from places where they had lots of conferences. And we thought, you guys don't have one here. There was one that used to come once in a while in Northern California, but I really don't remember very much about it. So we did. We started this. We went and we talked to all sorts of people.</p>
+
+                            <p>And there was a huge group of us that got together. And here we are. How many years later is this? It's amazing. What is it? 31? 31?
+
+                            <p>Wow. So I'm glad you're all here. And I'm glad that I'm still here, which is an absolute miracle, I have to tell you. When I came into the program, it was the last place I wanted to be. I have to tell you that. I thought, god, my life is over with now. And it's like the big book says, I really, truly thought my life was going to be boring and glum.</p>
+
+                            <p>I had a mother who had been in the program for a long time. And when her friends would come into the house, I would go out the back door. I would avoid them like the plague. But yet, when I came into the program, I wasn't sure I was an alcoholic. Now, mind you, I had had two marriages. I was in the midst of getting a divorce at that time. And I was a blackout drinker.</p>
+
+                            <p>And I still wasn't sure if I was an alcoholic. I thought I might be a little bit alcoholic, but not like all of you. I thought I'm still young. I might come in. I'll learn a little bit about it. And then I'll go out. And I'll come back when I'm older.</p>
+
+                            <p>You'll teach me how to drink a little bit. Because I would go out and I'd say, OK, I'm just going to have one drink. And I'd be closing down the dance floor. It was just horrible. And I thought, how does that happen to me? Well, it's because I've been alcoholic, of course. But anyway, I came into the program and I thought, I'll just stay here for a while.</p>
+
+                            <p>And I have to tell you, I believe I was love sober. Absolutely love sober. People gathered around me and they'd said to me, just keep coming back. Go to lots of meetings. Just keep coming back. Don't drink no matter what. And I thought, yeah, right.</p>
+
+                            <p>You don't understand me. I say I'm not going to drink. And then I drink. But they said, don't drink no matter what. And you know, I didn't pick up a drink. It was an absolute miracle. I just kept coming back.</p>
+
+                            <p>And I'll never forget, there was one day I was thinking, well, this will be my last day here for a while. And then I'll come back later. And this guy, he had 18 years and he had gone out. He drank again. And it scared me for some reason. Something happened to my soul. And I went up to him and I said, what happened?
+
+                            <p>Why, after 18 years, did you drink? And he said, I stopped going to meetings. And then something happened to me. And I thought, wow. So I started going to five meetings a day. I just didn't want to drink. And you guys got me.</p>
+
+                            <p>Something happened. And you got me. And here I am, 46 years later. And I haven't picked up a drink. And that is truly, truly a miracle because I've had all sorts of relationships that I thought I was going to kill myself over. But I didn't pick up a drink. And I just kept coming back.</p>
+
+                            <p>And I just, anyway, so what happened was I moved out here. And I moved out here in 83. And that's when I met Jerry. And he moved here later, actually, in 93. Yeah. And that's when we started this. But I was scared when I moved out here.</p>
+
+                            <p>And I went to a group and I didn't think they did it right. And I used to call my sponsor. I got sober and I got to see it. I used to call my sponsor. I'd say, I don't know how these people get sober out here. You know? I wanted to just get in the middle.</p>
+
+                            <p>And I wanted everybody to baby me because I was new. I wanted to feel like a newcomer again. And they did. They loved me. But I just wanted to be special anyway. But I just kept coming back. And they didn't have a 5.</p>
+
+                            <p>30 meeting either. And so I started that 5. 30. I don't say that out of ego. I say it because it saved my life. I needed a 5. 30 meeting because that's how I grew up in Alcoholics and Almat.</p>
+
+                            <p>And it's still going today. But anyway, as time went on, I just kept coming to meetings here. And I went to lots of meetings out here as well. And that for me is the key. But when I first came in, they wanted to talk about God. And I told them that when I got sober and I got my life straightened out, then I would talk about God. And I wanted to do stuff nine right away as well.</p>
+
+                            <p>And because I wanted to run around and tell everybody I was sorry. That's how I did my whole drinking, was telling everybody I was sorry. And this man came up to me and he patted me on the head. His name was Big Red. And he said, honey, just grow spiritually. And I told him, but you don't understand. My life is so screwed up.</p>
+
+                            <p>When I get my life straightened out, then I'll grow spiritually. And he said, no, you have to work the steps to grow spiritually. And that's what got me into the steps seriously. And I did, I know that stuff won't happen to me. It just automatically happened. And then in step two, being restored to sanity, well, I didn't think I was too insane. And then they said, no, insanity is for us as if we pick up a drink.</p>
+
+                            <p>And so I didn't want to be insane. So I didn't pick up a drink. And then step three, as they talked about, turning my will and my life over to the care of God, I, it was difficult, but I wanted what you guys had. But it was hard for me to surrender. It really was. And what happened was, I'll tell you exactly what happened. Melissa, you know this story.</p>
+
+                            <p>You were in the meeting the other day when I talked about it. And it is my turning point. It's my favorite story. I was in the midst of getting a divorce. And I was going to meetings where my husband that I was divorcing was on the 14th floor. And I used to go to meetings on the 30th floor. And I was in the elevator and I was going up.</p>
+
+                            <p>And he was kind of involved with his associate. And the door opened and there she stood on the 14th floor. And then the door closed. Needless to say, she didn't get on the elevator with me. And I went up to the 30th floor and I just said, that's it. I'm done. I'm going to kill him.</p>
+
+                            <p>That's it. I've got to kill him. And I told him exactly how I was going to do it. I was going to line him up and have a gun, of course. I don't know where I was going to get a gun. I don't even believe in guns. But that's where my brain was.</p>
+
+                            <p>And I said, I'm going to shoot her and she'll hit him. And they'll go out the window. I had it all planned out. And I told them. And they told me I had a resentment. I have to tell you, I really didn't know what resentment was. I'd never really heard about resentments.</p>
+
+                            <p>And they explained this guy grabbed me. Oh, first of all, of course, the topic was resentment. There was this long table with about 40 people. And it went around the room and everybody talked about resentment, how they handled it, what they did, and how important it was to do the right thing about resentment because we could drink over resentment and came to the last woman and she said, you know, I think this is justifiable resentment. And I'm in, yes. And this guy came up to me after the meeting and he grabbed me and he said, honey, if you don't pray for him or for both of them, you're probably going to drink it then you'll die. That's how resentful I was.</p>
+
+                            <p>Not even knowing what it was. He said, you keep refilling this over and over again. You talk about it in meetings. And you've got to do something about it. You have to pray for a minute. The next last story in the old book, in the old big book, I don't know where it is right now because the big books changed a bit. But I did.</p>
+
+                            <p>I did exactly what it said to do. I went home and I started praying for them both. And I prayed for two solid weeks. And they said, you do it once a day. Well, I did it twice a day. That's how sick I was. And it worked.</p>
+
+                            <p>It worked. And all of a sudden I felt love in my heart. I did not want him back. But there was this love that I had never felt before. It was so pure. And that's when I knew that this thing worked. And it made it easier for me to take step three.</p>
+
+                            <p>So I did. And then I had to do step four. And I avoided it for a long time. But when the pain got bad enough, I did step four. And then I shared it with my sponsor. And she had me look at myself. And it was all about me.</p>
+
+                            <p>You know, it was every time something came up, there was something that was about me why I was resentful, you know, or why I was angry. And where am I? Am I talking too long? Yeah. I have one minute. Holy God, I'm only on step three. Anyway, I have to tell you another thing that happened is when I had, actually it was the end of 93.</p>
+
+                            <p>Right at the end, in September, my daughter passed away in 1993. And I always said that the only thing I would ever drink over was if something happened to one of my children. The miracle is the thought of a drink never came to me. And that's a miracle in alcoholics and alamists. It really is. And I'm so grateful for that. You guys, you loved me.</p>
+
+                            <p>You kept me sober. And I keep coming back because of you. I see so many of you here that I'm sitting in meetings with. And you know how much I love you. You know, really. And Chris, thank you again. I adore you, you know.</p>
+
+                            <p>And you're doing such a great job here, honestly. You know, I need to congratulate you because you just took it and boom. And all the people that are doing the whole thing with you. You know, it's great. So I could go on and on and on. I know I jump around. I go back and forth.</p>
+
+                            <p>But I'm grateful to be here. This is not my favorite thing to do. You know, it doesn't matter how long you've got. You know, I do service in other ways. I sponsor a lot of women. You know, and they say the women stick with the men, stick with the men in sponsorship. But that's what I learned.</p>
+
+                            <p>And that's what I do. And that's how I do service. You know, I show up and I go to lots of meetings. And my phone is usually with me. So people can usually get a hold of me. But I guess God had other ideas for me today. So anyway, thank you so much.</p>
+
+                            <p>I'm so happy you're all here. And just keep coming back. That's what they told me. Just keep coming back. But I'm so glad I believed them. And my life got better. My life is so good today.</p>
+
+                            <p>You know, I just want to yell. Yippee. Thank you.</p>
                         </section>
                     </div>
                 </div>
