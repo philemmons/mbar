15a72b9e4ecb4cce0e37d16eb298a875daa25596
philemmons <pemmons@csumb.edu>
2024-10-09 12:45:24 -0700

    mike complete es en

--- con-2024-mike.php
+++ con-2024-mike.php
@@ -27,7 +27,63 @@ include_once 'header-bottom.inc';
                         <section aria-label="transcript">
                             <h3 class="text-center p-4">Below is a raw transcript from the audio recording by<br>Mike V. in 2024.<span class="sr-only">(Click to go back)</span>
                             </h3>
-                            <p> Transcript Coming Soon </p>
+                            <p>Thank you. My name is Mike. I'm an alcoholic. Good to see everybody. I was told the format was a different format and then I got here and my plans went out the window so.</p>
+
+                            <p>Anyway, it's probably good. My sobriety dates July 14 1997 so I've been sober a little over 27 years and. You know what I do daily. It's it's I guess kind of it's kind of hard for me to put my finger on you know first and foremost is. Besides besides making a cup of coffee first thing out of bed.</p>
+
+                            <p>Is I head to a meditation chair that I have so I've got this this one set chair and and I've been using that chair for a long time. And I got you know a few different spiritual books. You know a couple of may a books another one that's an on a but it's spiritual related stuff. And and so I sit down and I pray and I you know. I read those books as a form of prayer and then I get quiet and I meditate for a bit and.</p>
+
+                            <p>Just try and prove my conscious contact with God, you know for the day and and get in touch. again, you know and the other thing that that. That comes to mind is is that I have constant contact with other alcoholics. One fashion or another, you know on most days. and.</p>
+
+                            <p>That's an unbelievable reminder for me, you know, I mean look I was told early on the only step. I have to do perfectly a step one, you know, and. And I get reminded on a regular basis, you know. Whether I'm talking to somebody that's got time or whether I'm talking to somebody that's new I. Get reminded on a regular basis, you know.</p>
+
+                            <p>Who and what I really am and that's a guy that cannot drink safely. I've got a lot of colorful stories about the chaos. that what my life was like before I stopped drinking and. And by being in the middle of alcoholics anonymous I get you know. Not only to be of service not only give something away.</p>
+
+                            <p>But but I just get this constant contact with other people and and I get people that you know. Like I may have a problem and I share it with one of my sober friends. Here's what's going on and and I may you know give us some diatribe or something and then they'll remind me they'll go. Well, you know pray about it and see what comes back and like oh god, that's right. You know, I'm human, you know, and and so and then there's other days where I'm reminding somebody else, you know.</p>
+
+                            <p>It's like okay. Well, where's God met and and you know this gift that that that we give each other on an ongoing basis, you know like that is. It's pretty remarkable, you know, and and I you know. I look at a lot of normies that that you know, they don't they don't have what we have, you know. They don't have this constant reminder that that we get and.</p>
+
+                            <p>You know, I. Think I probably learned everything everything that I know about you know getting sober and staying sober in my first six months for my first. Year I had a sponsor who suggested that I start praying when I was probably I don't know two three weeks over or something like that. You know, he suggested I go home and. And ask God to keep me sober for the day and and at night before I go to bed, you know.</p>
+
+                            <p>get on my knees and say thanks and and so I started doing that and. And then I I was surrounded by a bunch of people that were active in AA, you know. People that went to meetings on a regular basis people that sponsored other people. I'd show up at a meeting and you know one of these guys to be in the car, you know. With a new guy and they're sitting there reading that reading a step before the meeting and so I think most of the stuff.</p>
+
+                            <p>You know other than going through the steps, you know with my sponsor. But but most of the stuff that I I've learned and I do an AA. It's because I was shown it's not because I was told I there's people that I looked up to and and they were doing it. You know, although, you know when I was new, you know. I did I remember getting the elbow and it's like that coffee commitment.</p>
+
+                            <p>That's gonna be good for you, you know, and and then later on. It's to get the elbow from my sponsor get it that secretary position. Yeah. Yeah. You should be raising your hand for that so yeah.</p>
+
+                            <p>I did have you know a little nudge here and there but but it was it was from people that were doing that stuff, right? . and. And I thank God for the willingness that I've I've been willing you know all this time to keep coming back. And to keep putting myself in the middle of AA so that I get opportunities like this if I wasn't in the middle of.</p>
+
+                            <p>AA nobody would have nobody would have thrown my hat in the basket, you know to come up here and speak, right or. So. You know a couple practical examples maybe. You know. Wednesday night I was in a meeting and and there was a there was a young guy in there with about 60 days sober.</p>
+
+                            <p>who was. Who who you know he shared and he and he said that he was getting ready to you know travel home to four or Texas and. You know at 60 days sober. He's scared that he's not gonna be able to stay sober when he gets back home. And so you know I got experience with moving with moving in AA and staying sober and I got people that coached me.</p>
+
+                            <p>You know before I moved here from Orange County and. And so I grabbed him I beeline form after the meeting took him aside spent some time with him got a phone number. Gave him a couple of practical suggestions and then I've been I've been calling him last couple days, you know and talking to him. What is that you know I hopefully it's helpful for him, you know, but it's helpful for me. I can tell you that right and and it reminds me of of what I need to continue to do.</p>
+
+                            <p>You know another one so so I just right before I walked over here. I had I had coffee. With with a friend of mine who's who's visiting from Orange County. And this guy I met him a little over 19 years ago when I was in a meeting in Laguna Beach. And and this guy was like two weeks over and and I heard him share how he was going to travel the Monterey to visit family.</p>
+
+                            <p>And he was concerned that he wouldn't be able to stay sober when he was up here visiting his family. So I beeline for him and I and I said hey, man, you know, I got family Monterey. I go to meetings up there, you know, you know here here's you know, where's he live? . You know this meeting that meeting and I got his number well.</p>
+
+                            <p>Me and this guy, you know for the past, you know, his he's got a little over 19 years, but but we've been great friends ever since now. How does that happen? I don't know just I heard the some guy needed a little a little, you know. Suggestion maybe about where to find meetings in Monterey, you know 19 years ago and. And I've had times since I've been living up here a dozen years or so that every time I go down to Orange County.</p>
+
+                            <p>He's one of the guys I stay with, you know, and he said it again. He goes hey, man when you come down. He goes, you know, stay with me. And it's like those kinds of friendships that I found and I continue to find in Alcoholics Anonymous, you know. It's amazing.</p>
+
+                            <p>I mean people that I could call day night, man. These people will come for me in the middle of the night. I need something and I do the same thing for them. You know, it's amazing it's amazing that a guy I live my life like a bull in a china shop. And if I told you some of my drinking stories you'd go that guy sick.</p>
+
+                            <p>But it's like I you know, I got this I got this relationship with the higher power. I've got a God in my heart and I've got compassion and it's like I only got it from being around Alcoholics Anonymous. I only got it from other people that live their lives like that too. I mean which is amazing to me. So so what we're having coffee.</p>
+
+                            <p>Jerry tells me about our other friend. who. he moved down to Orange County and. And he had five DUIs and and you know, felony DUIs and he's looking at going away to prison, you know, and and. And so I I became very good friends with this guy and.</p>
+
+                            <p>I don't know he put together seven eight years and we spent a lot of time together. He didn't have a driver's license. So I drove him to a lot of meetings, you know. When we we both lived in Laguna Beach and and so this guy moved back to Fresno. He never he never found a home.</p>
+
+                            <p>Back in AA when he moved to Fresno, you know, he went to meetings got a sponsor. but he didn't really find a home and and he and he drifted away and he drank again and. And this this friend of mine that I had coffee today. He was actually his sponsor, you know when those five six years that he lived in in Laguna Beach. And he told me how he got a call from this guy two weeks ago and and he's drinking and I.</p>
+
+                            <p>Mean I know he's been drinking for quite a while. He's been drinking five six years and I've been in touch with him somewhat. But he called him and said hey, I just picked up another felony DUI. So now he's got two fresh DUIs that he's going to court for. the wife has.</p>
+
+                            <p>You know filed for separation, you know, they got a couple kids that are in the middle of all that and. You know, this guy's part of an ag family and they they farm thousands of acres and so from the money standpoint everything's fine. But the rest of it, you know, it's like it's falling apart, you know, and so I'm gonna be calling him this afternoon and saying hey, man, you know. Trying to see if I can help him, you know. He's he's called me before with his problems.</p>
+
+                            <p>And he wanted to come out here and stay for a weekend and hit meetings or you know. And maybe I'll go out there and stay with him for a week and hit meetings. I don't know and prop them up but you know, I've had a lot of people prop me up over the years and and I've been able to prop up a lot of other people and. It's it's an incredible gift that. That I've been given I think that we're all given here.</p>
+
+                            <p>So. God willing I'll be able to stay active and and keep doing it, you know one day at a time. Thank you.</p>
                         </section>
                     </div>
                 </div>
