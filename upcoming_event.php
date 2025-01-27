587f8203fa8e03a852c33695c445ece1c5717cdd
philemmons <phil_emmons@sbcglobal.net>
2025-01-26 16:07:17 -0800

    test navbar

--- upcoming_event.php
+++ upcoming_event.php
@@ -41,7 +41,7 @@ include_once 'header-bottom.inc';
                          <button class="nav-link dropdown-toggle" type="button" id="dropdown-upcoming-events" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Upcoming Events</button>
                          <div class="dropdown-menu" aria-labelledby="dropdown-upcoming-events">
                               <a class="dropdown-item" href="logo_contest.php">Logo Contest</a>
-                              <a class="dropdown-item active" aria-current="page" href="upcoming_event.php">Gratitude<span class="visually-hidden">(current)</span></a>
+                              <a class="dropdown-item active" aria-current="page" href="upcoming_event.php">St. Patrick's Day Potluck>(current)</span></a>
                          </div>
                     </li>
                     <li class="nav-item">
