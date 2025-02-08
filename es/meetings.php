bb57aa99d9cbf7138ad797f5a789bfcabb6f4ebf
philemmons <phil_emmons@sbcglobal.net>
2025-02-08 09:58:19 -0800

    content

--- es/meetings.php
+++ es/meetings.php
@@ -74,8 +74,7 @@ include_once 'header-bottom.inc';
         <div class="container shadow-wrap">
             <div class="row justify-content-center mb-5">
                 <div class="col-sm-auto py-4">
-                    <a class="btn btn-primary btn-sm" href="Join Zoom Meeting
-https://us02web.zoom.us/j/3301007207?pwd=olR3pXbrM9zKtXLjhdVqFPfOtbbbdh.1&omn=87244042787" rel="noopener noreferrer nofollow" target="_blank"><img class="img-fluid" src="../images/zoom-login.png" alt="Pantalla de inicio de sesión de Zoom invertida con fondo azul."><span class="sr-only">(Abre la aplicación Zoom)</span></a>
+                    <a class="btn btn-primary btn-sm" href="https://us02web.zoom.us/j/3301007207?pwd=olR3pXbrM9zKtXLjhdVqFPfOtbbbdh.1&omn=87244042787" rel="noopener noreferrer nofollow" target="_blank"><img class="img-fluid" src="../images/zoom-login.png" alt="Pantalla de inicio de sesión de Zoom invertida con fondo azul."><span class="sr-only">(Abre la aplicación Zoom)</span></a>
                 </div>
                 <div class="col-md-8 py-4">
                     <div class="p-3 text-bg-light hero-text-border text-center">
