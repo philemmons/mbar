3d2056ab08918ace498b6079d9e457150efa11ff
philemmons <phil_emmons@sbcglobal.net>
2025-02-08 20:02:53 -0800

    content; layout

---
+++ al-anon-with-aa-event.php
@@ -0,0 +1,155 @@
+<?php
+session_start();  //start or resume an existing session
+include_once 'header-top.inc';
+echo "<title>MBAR - Al-Anon with A.A. Participation</title>";
+echo '<link rel="canonical" href= "https://montereybayarearoundup.org/al-anon-with-aa-event.php" >';
+include_once 'header-bottom.inc';
+?>
+
+<nav class="navbar navbar-expand-lg" aria-label="main navigation">
+    <div class="container">
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-conference" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conference</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-conference">
+                        <a class="dropdown-item" href="conference-2024.php">MBAR 2024</a>
+                        <a class="dropdown-item" href="aa-spanish-marathon-2024.php">Spanish-Speaking A.A. Marathon Meetings</a>
+                        <a class="dropdown-item" href="aa-marathon-2024.php">A.A. Marathon Meetings</a>
+                        <a class="dropdown-item" href="al-anon-marathon-2024.php">Al-Anon Marathon Meetings</a>
+                        <a class="dropdown-item" href="register-now.php">Registration</a>
+                        <a class="dropdown-item" href="activities.php">Activities</a>
+                        <a class="dropdown-item" href="mbar_history.php">MBAR History</a>
+                        <a class="dropdown-item" href="memories.php">Memories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-who-we-are" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Who We Are</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-who-we-are">
+                        <a class="dropdown-item" href="our_partner.php">Our Partner</a>
+                        <a class="dropdown-item" href="meetings.php">Meetings</a>
+                        <a class="dropdown-item" href="committees.php">Committees</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle" type="button" id="dropdown-upcoming-events" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Upcoming Events</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-upcoming-events">
+                        <a class="dropdown-item" href="logo_contest.php">Logo Contest</a>
+                        <a class="dropdown-item" href="upcoming_event.php">St. Patrick's Day Potluck<span class="visually-hidden">(current)</span></a>
+                        <a class="dropdown-item active" aria-current="page" href="al-anon-with-aa-event.php">Al-Anon with A.A. Participation</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item" style="border-right: none;">
+                    <a class="nav-link" href="helping_hands.php">Helping Hands</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main id="main-content">
+    <!-- Hero Part -->
+    <div class="container shadow-wrap">
+        <div class="row justify-content-center py-6 mb-5 bg-body-tertiary bg-img-participation" title="Gratitude rock on a bed of Leaves">
+            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
+                <div class="p-3 text-center text-bg-light hero-text-border" title="Al-Anon and A.A. Event">
+                    <h2 class="display-6 fw-bold px-3 px-md-0">Al-Anon with A.A. Participation</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Part One -->
+    <article aria-label="Current event">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-8 col-lg-8 col-md-10 py-4">
+                    <div class="p-3 text-center text-bg-light hero-text-border">
+                        <section aria-label="Event Information">
+                            <h3 class="card-title mb-4">
+                                <img src="images/icons/alanon-icon.png" width="30" height="30" alt="Al-Anon icon">
+                                <strong>Monday Night Serenity - 41st Anniversary</strong>
+                                <img src="images/icons/aa-icon.png" width="30" height="30" alt="A.A icon">
+                                <p>"Honoring the Past, Embracing the Future"</p>
+                            </h3>
+                        </section>
+                        <img class="img-fluid" src="../images/al-anon-with-aa-event.png" alt="Al-Anon with A.A. Participation flyer">
+                    </div>
+                </div>
+            </div>
+    </article>
+
+    <!-- Part Two -->
+    <article aria-label="Event details">
+        <div class="container shadow-wrap">
+            <div class="row justify-content-center mb-5">
+                <div class="col-xl-8 col-lg-8 col-md-10 py-4">
+                    <div class="p-3 text-bg-light hero-text-border">
+                        <p>Monday, February 17th</p>
+                        <div class="row fw-bold">
+                            <div class="col-md-6 col-sm-12 mb-3 mb-sm-0">
+                                <p class="">
+                                    5:30pm
+                                    <br>
+                                    Doors Open
+                                    <br>
+                                    Light Refreshments
+                                    <br><br>
+                                    6:00pm
+                                    <br>
+                                    Meeting Commences
+                                    <br><br>
+                                    7:15pm
+                                    <br>
+                                    Al-Anon Speaker: Father Tom W.
+                                    <br>
+                                    Oakland, CA.
+                                </p>
+                            </div>
+                            <div class="col-md-6 col-sm-12 mb-3 mb-sm-0">
+                                <p class="">
+                                    6:30pm
+                                    <br>
+                                    A.A. Speaker: Melanie Y.
+                                    <br>
+                                    Sand City, CA.
+                                    <br><br>
+                                    7:00pm
+                                    <br>
+                                    Break
+                                    <br><br>
+                                    8:15pm
+                                    <br>
+                                    Cake
+                                    <br>
+                                    Fellowship
+                                </p>
+                                </p>
+                            </div>
+                        </div>
+                        <p class="text-center text-dark ">
+                            First Presbyterian Church of Monterey, 501 El Dorado, Monterey, CA
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+
+    <?php include_once 'event-gratitude.inc' ?>
+
+    <div id="map"></div>
+
+    <?php include_once 'footer.inc' ?>
+    </body>
+
+    </html>
